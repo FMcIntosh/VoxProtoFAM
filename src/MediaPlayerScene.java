1bdde1ffbf0c8a985b281fdafe4c57b089f0b414
Fraser McIntosh <frasermcintosh1@gmail.com>
2016-09-20 17:43:46 +1200

    Make video show in new window and size to window to video

--- src/MediaPlayerScene.java
+++ src/MediaPlayerScene.java
@@ -1,14 +1,20 @@
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.File;
@@ -28,27 +34,65 @@ public class MediaPlayerScene {
 
 
         final Slider slider = new Slider();
-        final VBox vbox = new VBox();
-        vbox.getChildren().add(slider);
-        root.getChildren().addAll(view, vbox);
+        final VBox sliderBox = new VBox();
+        final HBox controlLayout = new HBox();
+        final VBox controls = new VBox();
+        sliderBox.getChildren().add(slider);
+
+
+
+        Button stop= new Button("Stop");
+        stop.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                player.stop();
+            }
+        });
+
+        Button pause = new Button("Pause");
+        pause.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                player.pause();
+            }
+        });
+
+        Button play = new Button("play");
+       play.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                player.play();
+            }
+        });
+
+        controlLayout.setAlignment(Pos.CENTER);
+        controlLayout.getChildren().addAll(stop, pause, play);
+        controlLayout.setTranslateY(330);
+        controls.getChildren().addAll(sliderBox, controlLayout);
+        root.getChildren().addAll(view,controls);
 
 
 
 
 
-
+        final Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
         Scene mediaScene = new Scene(root, AppModel.getWidth(), AppModel.getHeight());
-        AppModel.setScene(mediaScene);
+        window.setScene(mediaScene);
+        window.setResizable(false);
+        window.show();
 
-       player.play();
+        player.play();
         player.setOnReady(new Runnable() {
             @Override
             public void run() {
                 int w = player.getMedia().getWidth();
                 int h =player.getMedia().getHeight();
 
-                vbox.setMinSize(w, 100);
-                vbox.setTranslateY(h - 70);
+                window.setMinWidth(w);
+                window.setMinHeight(h);
+                sliderBox.setMinSize(w, 100);
+                sliderBox.setTranslateY(h - 70);
 
                 slider.setMin(0.0);
                 slider.setValue(0.0);
